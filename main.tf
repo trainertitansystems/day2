09ea9444010ad045c96beef209ecdd2f58b688cc
Sujad Syed <shaz@Sujads-MacBook-Air.local>
2024-08-07 20:19:55 +0530

    7thaugterraformfiles

---
+++ main.tf
@@ -0,0 +1,16 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
